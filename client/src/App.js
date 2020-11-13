3403aa4e96ec55b7773ab42f2ef688163073bbf3
nguyendm77 <nguyendm77@gmail.com>
2020-11-13 14:24:46 -0800

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
-
+import { Footer } from './components/Footer';
 import { GlobalProvider } from './context/GlobalState';
 
 import './App.css';
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <h3>
+    <h3  className="footer">
       Developed by Minh Nguyen @2020.
     </h3>
   )
